946dcd48d711ff799d1247a6da69c9893488e1ae
pedropassos386-source <pedropassos386@gmail.com>
2026-06-17 15:05:16 -0300

    novas mudanças no midia para mobile

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -32,25 +32,30 @@ const depoimentos = [
     {
         nome: "Felipe Caldas Pereira",
         cargo: "Sub-Mestre / Jogador Veterano",
-        foto:"./imagens/felipe.jpeg",
+        foto:"./imagens/felipe.webp",
         texto: "Jogar uma segunda campanha na mesa de um amigo é diferente. Na primeira, tudo é novidade; na segunda, tudo traz lembranças. Voltar com a filha do meu antigo personagem faz cada lugar e história terem um peso emocional maior. Ela não parece só “mais uma ficha”. Carrega o legado do pai enquanto tenta criar a própria identidade. E é isso que torna o RPG especial: não são só dados ou combates, mas memórias e emoções construídas ao longo dos anos. No fim, a campanha acaba crescendo junto com a gente."
     },
 
     {
         nome: "Danilo Augusto",
         cargo: "Jogador Veterano",
-        foto:"./imagens/rosa.jpg",
+        foto:"./imagens/rosa.webp",
         texto: "Jogar a mesa do Pedro é uma experiência divertida. Não foram raros os momentos que em uma hora ele era o cara mais bacana do mundo, e na outra eu queria voar no pescoço dele. Mas brincadeiras a parte, é bastante divertido, tanto dentro quanto fora da mesa, lembro que antigamente, todo final de sessão, a gente fazia um bocado de memes sobre oque tinha ( particularmente os meus eram os mais engraçados, mas nenhum dos outros tem que saber disso ). Se você procura um espaço bacana, amigável e tá afim de perder um pouco de sanidade, essa mesa é pra você."
     },
 
     {
         nome: "Pedro Passos",
         cargo: "Mestre / Criador do In the Mind",
-        foto:"./imagens/akatsuki.jpeg",
+        foto:"./imagens/akatsuki.webp",
         texto: "Quando criei este mundo, não imaginava que ele realmente daria certo. Mas, ao ver os jogadores se divertindo, rindo e criando momentos inesquecíveis juntos, eu finalmente entendi o que significa ser mestre de RPG. A partir daquele momento, as loucuras se tornaram cada vez mais comuns, e cada sessão passou a trazer histórias que jamais poderiam ser planejadas. Afinal, como um dos jogadores costuma dizer: 'O dia mais louco nesta mesa é apenas mais um dia comum.' "
     },
 
-
+    {
+        nome: "Felipe Porto",
+        cargo: "Jogador Veterano",
+        foto:"./imagens/take.webp",
+        texto: "Jogar na mesa do Pedro é algo que traz para mim a essência de um RPG de mesa, onde vários amigos se reúnem e enfrentam dificuldades juntos. Não é um jogo fácil, mas é 100% recompensador",
+    }
 ];
 
 const bullets = document.querySelectorAll(".slider-bullets span");
